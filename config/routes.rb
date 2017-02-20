cba84f4e36a5244ec5c9681ff9d452282f953028
EDLyonhart <EDLyonhart@gmail.com>
2017-02-20 12:14:16 -0800

    work on Sessions, logins, etc...

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,28 @@
 Rails.application.routes.draw do
-  get 'users/new'
 
-  get 'users/show'
+  root 'users#new'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+
+  get    '/signup',  to: 'users#new'
+
+  get    '/login',   to: 'login_sessions#new'
+  post   '/login',   to: 'login_sessions#create'
+  delete '/logout',  to: 'login_sessions#destroy'
+  
 end
+
+
+#    Prefix Verb   URI Pattern               Controller#Action
+#     users GET    /users(.:format)          users#index
+#           POST   /users(.:format)          users#create
+#  new_user GET    /users/new(.:format)      users#new
+# edit_user GET    /users/:id/edit(.:format) users#edit
+#      user GET    /users/:id(.:format)      users#show
+#           PATCH  /users/:id(.:format)      users#update
+#           PUT    /users/:id(.:format)      users#update
+#           DELETE /users/:id(.:format)      users#destroy
+#    signup GET    /signup(.:format)         users#new
+#     login GET    /login(.:format)          login_sessions#new
+#           POST   /login(.:format)          login_sessions#create
+#    logout DELETE /logout(.:format)         login_sessions#destroy
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,13 +1,18 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+
+  setup do
+    @user = users(:one)
+  end
+
   test "should get new" do
-    get users_new_url
+    get new_user_path
     assert_response :success
   end
 
   test "should get show" do
-    get users_show_url
+    get user_path(@user.id)
     assert_response :success
   end
 
