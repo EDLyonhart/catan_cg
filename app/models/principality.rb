91cb91985fc702f6ca154553a5817e33647e68bc
EDLyonhart <EDLyonhart@gmail.com>
2017-02-22 10:06:53 -0800

    principality layout bones sketched out

--- app/models/principality.rb
+++ app/models/principality.rb
@@ -4,4 +4,35 @@ class Principality < ApplicationRecord
   belongs_to :player
   has_many :cards
 
+  serialize :layout, Array
+
+  def starting_layout
+
+    self.layout = [
+                    { center: nil, first_up: nil, first_down: nil, second_up: nil, second_down: nil },
+                    { center: nil, first_up: nil, first_down: nil, second_up: nil, second_down: nil },
+                    { center: nil, first_up: nil, first_down: nil, second_up: nil, second_down: nil },
+                    { center: nil, first_up: nil, first_down: nil, second_up: nil, second_down: nil },
+                    { center: nil, first_up: nil, first_down: nil, second_up: nil, second_down: nil },
+                    { center: nil, first_up: nil, first_down: nil, second_up: nil, second_down: nil },
+                    { center: nil, first_up: nil, first_down: nil, second_up: nil, second_down: nil },
+                    { center: nil, first_up: nil, first_down: nil, second_up: nil, second_down: nil },
+                    { center: nil, first_up: nil, first_down: nil, second_up: nil, second_down: nil },
+                    { center: "available", first_up: "available", first_down: "available", second_up: nil, second_down: nil },
+                    { center: "city", first_up: "available", first_down: "available", second_up: nil, second_down: nil },
+                    { center: "road", first_up: "available", first_down: "available", second_up: nil, second_down: nil },
+                    { center: "city", first_up: "available", first_down: "available", second_up: nil, second_down: nil },
+                    { center: "available", first_up: "available", first_down: "available", second_up: nil, second_down: nil },
+                    { center: nil, first_up: nil, first_down: nil, second_up: nil, second_down: nil },
+                    { center: nil, first_up: nil, first_down: nil, second_up: nil, second_down: nil },
+                    { center: nil, first_up: nil, first_down: nil, second_up: nil, second_down: nil },
+                    { center: nil, first_up: nil, first_down: nil, second_up: nil, second_down: nil },
+                    { center: nil, first_up: nil, first_down: nil, second_up: nil, second_down: nil },
+                    { center: nil, first_up: nil, first_down: nil, second_up: nil, second_down: nil },
+                    { center: nil, first_up: nil, first_down: nil, second_up: nil, second_down: nil },
+                    { center: nil, first_up: nil, first_down: nil, second_up: nil, second_down: nil },
+                    { center: nil, first_up: nil, first_down: nil, second_up: nil, second_down: nil }
+                  ]
+  end
+
 end
