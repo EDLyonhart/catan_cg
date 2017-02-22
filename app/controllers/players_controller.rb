b9e0f828286b3b905108395f021e31ad43efd597
EDLyonhart <EDLyonhart@gmail.com>
2017-02-22 10:31:53 -0800

    include player_params

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,10 +1,25 @@
 class PlayersController < ApplicationController
+
   def new
+    @player = Player.new
   end
 
   def create
+    player = Player.new(player_params)
+    if player.save
+      log_in player
+      redirect_to player
+    else
+      render 'new'
+    end
   end
 
   def update
+  end
+
+  private
+
+  def player_params
+    params.require(:player).permit( :user_id, :game_id, :victory_points )
   end
 end
