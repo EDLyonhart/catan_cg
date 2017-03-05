713b81a431f7f5d34f612e1ebf66cf462e216e40
EDLyonhart <EDLyonhart@gmail.com>
2017-03-04 17:52:31 -0800

    add on_create actions

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,8 +1,21 @@
 class Game < ApplicationRecord
 
+  require 'pry'
+
   has_many :players
   has_one :gameboard
   has_one :turn
+
+  after_save :create_gameboard
+  after_save :create_turn
+
+  def create_gameboard
+    Gameboard.create(game_id: self.id, remaining_settlements: 5, remaining_cities: 7, remaining_roads: 7)
+  end
+
+  def create_turn
+    Turn.create(game_id: self.id, number: 0, phase: "roll")
+  end
 
 
   def self.players_by_name(game_id)
